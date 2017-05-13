58ff808060cdd90a589520af4f23f65451e30c67
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Pro.local>
2017-05-12 21:27:12 -0500

    Initial Commit

---
+++ codeChef/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  codeChef
+//
+//  Created by Abhishek Kumar on 5/12/17.
+//  Copyright © 2017 Abhishek Kumar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
